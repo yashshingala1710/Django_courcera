73c1d83bac13f8362af509693f4f709999d10f2c
yashshingala1710 <yashshingala1710@gmail.com>
2026-03-22 17:53:09 +0530

    Add URL patterns for submit and result views

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('submit/<int:course_id>/', views.submit, name='submit'),
+    path('result/<int:submission_id>/', views.show_exam_result, name='show_exam_result'),
+]
